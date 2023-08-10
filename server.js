dd03606dc62c49042b9cd54e772ea28929c90a49
Pratyush <pratyushojha059@gmail.com>
2023-08-10 22:39:57 +0545

    Working with setting up axios

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const userRoutes = require('./src/users/routes');
 const itemRoutes = require('./src/items/routes')
+const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = 8000;
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    // credentials: true,
+    // optionsSuccessStatus: 204,
+}))
 app.use(express.json());
 
-app.use('/api/users' , userRoutes);
-app.use('/api/items' , itemRoutes);
 
-app.listen(port ,() => console.log(`app listening on port ${port}`));
+app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
+app.listen(port, () => console.log(`app listening on port ${port}`));
